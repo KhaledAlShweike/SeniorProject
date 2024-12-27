15edc73f8ecb038351ea8b5777ed3475d14347ee
KhaledAlShweike <khaledshwike.01@gmail.com>
2024-12-27 15:19:05 +0300

    Setting up the DB

---
+++ database/migrations/2024_12_26_195848_create_doctors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\Tenant;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->text('password');
+            $table->string('phone_number');
+            $table->string('photo');
+            $table->string('role');
+            $table->string('specialization');
+            $table->string('clinic_address');
+            $table->foreignIdFor(Tenant::class)->onDelete('set null')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
