38ee9c93e4ed62dd08a7a4edddf730104a5bd529
Omaralaj <alajatiomar57@gmail.com>
2024-12-30 11:51:15 +0300

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+
+});
