c9958ca29f59edd0ce8f30e0316fc4ee69347d41
KhaledAlShweike <khaledshwike.01@gmail.com>
2025-01-13 01:33:11 +0300

    0

--- config/database.php
+++ config/database.php
@@ -29,12 +29,12 @@ return [
     |
     */
 
-    'connections' => [
-    'mysql' => [ // Default database
+  'connections' => [
+    'mysql' => [ // First database (EHR_DB)
         'driver' => 'mysql',
         'host' => env('DB_HOST', '127.0.0.1'),
         'port' => env('DB_PORT', '3306'),
-        'database' => 'EHR_DB', // Update here
+        'database' => env('DB_DATABASE', 'EHR_DB'),
         'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', ''),
         'charset' => 'utf8mb4',
@@ -43,11 +43,11 @@ return [
         'strict' => true,
         'engine' => null,
     ],
-    'mysql2' => [ 
+    'mysql2' => [ // Second database (MIR_DB)
         'driver' => 'mysql',
         'host' => env('DB_HOST_SECOND', '127.0.0.1'),
         'port' => env('DB_PORT_SECOND', '3306'),
-        'database' => 'MIR_DB',
+        'database' => env('DB_DATABASE_SECOND', 'MIR_DB'),
         'username' => env('DB_USERNAME_SECOND', 'root'),
         'password' => env('DB_PASSWORD_SECOND', ''),
         'charset' => 'utf8mb4',
@@ -57,6 +57,8 @@ return [
         'engine' => null,
     ],
 ],
+
+
 
     /*
     |--------------------------------------------------------------------------
